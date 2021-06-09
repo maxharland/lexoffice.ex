e0c3be193e43c0ab10841261290f8363cdd89ccb
Franz Bettag <franz@bett.ag>
2021-06-09 08:41:13 +0200

    fixes InvoiceDetailsResponse deserialization for 0.1.4.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule LexOffice.Mixfile do
   def project do
     [
       app: :lexoffice,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.7",
       source_url: @project_url,
       homepage_url: @project_url,
